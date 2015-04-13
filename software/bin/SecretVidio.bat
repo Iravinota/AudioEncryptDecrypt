440ed93ea7dae3cddcfe764e3bac370c3cbb1e81
Iravinota <wswangshuo@gmail.com>
2015-04-13 10:01:48 +0800

    Change bat file for optimizing

--- software/bin/SecretVidio.bat
+++ software/bin/SecretVidio.bat
@@ -1,3 +1,10 @@
 @echo off
+set JARS="./*.jar"
+set LIBPATH=.
 
+setlocal EnableDelayedExpansion
+FOR %%i IN (%JARS%) DO SET CLASSPATH=!CLASSPATH!;%LIBPATH%/%%i
+echo %CLASSPATH%
 start javaw -cp AudioEncryptDecrypt.jar com.ws.project.dencrypt.gui.DencryptUI
+
+endlocal
